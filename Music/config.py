5ca4a004397dc74b5992c7c960f781af18878a09
osamaapdalla099 <107728830+osamaapdalla099@users.noreply.github.com>
2022-07-14 00:37:52 +0200

    Add files via upload

--- Music/config.py
+++ Music/config.py
@@ -1,21 +1,28 @@
-##Config
-
+import os
 from os import getenv
 from dotenv import load_dotenv
 
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
 load_dotenv()
-get_queue = {}
-SESSION_NAME = getenv('SESSION_NAME', 'etalee')
-BOT_TOKEN = getenv('BOT_TOKEN')
-API_ID = int(getenv('API_ID', "9953702"))
-API_HASH = getenv('API_HASH')
-DURATION_LIMIT = int(getenv('DURATION_LIMIT', '30'))
-COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '').split())
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-SUDO_USERS = list(map(int, getenv('SUDO_USERS', '').split()))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", '-1001732849713'))
-ASS_ID = int(getenv("ASS_ID", '5363951902'))
-OWNER_ID = list(map(int, getenv('OWNER_ID', '').split()))
-BOT_IMG = getenv("BOT_IMG")
-GROUP = getenv("GROUP", None)
-CHANNEL = getenv("CHANNEL", None)
+admins = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME", "")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+OWNER_NAME = getenv("OWNER_NAME", "")
+ALIVE_NAME = getenv("ALIVE_NAME", "")
+BOT_USERNAME = getenv("BOT_USERNAME", "")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "").split())
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph//file/fc3205a6fbb82dd994412.jpg")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "")
+IMG_1 = getenv("IMG_1", "https://telegra.ph//file/fc3205a6fbb82dd994412.jpg")
+IMG_2 = getenv("IMG_2", "https://telegra.ph//file/fc3205a6fbb82dd994412.jpg")
+IMG_3 = getenv("IMG_3", "https://telegra.ph//file/fc3205a6fbb82dd994412.jpg")
